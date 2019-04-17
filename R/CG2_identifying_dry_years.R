da1eda66eebecd4a796e33adaec2e972b3068aa0
Colin Byrne <Colin@campus-096-190.ucdavis.edu>
2019-04-17 09:08:51 -0700

    Adding CG2 code

--- R/CG2_identifying_dry_years.R
+++ R/CG2_identifying_dry_years.R
@@ -2,7 +2,7 @@
 library(ggplot2)
 
 ### USER DEFINED FUNCTIONS ###
-read_streamflow_data <- function(file = './data/sacramento-bendbridge-paleo.csv'){
+read_streamflow_data <- function(file = '../data/sacramento-bendbridge-paleo.csv'){
 	df <- read.csv(file, header = TRUE)
 	return(df)
 }
@@ -13,14 +13,78 @@ df <- read_streamflow_data()
 
 # TODO
 
+dryPeriods <- function(flowDf, yrWindow, dryFlowMin, dryFlowMax, totToggle) {
+  
+  # Figure out the minimum and maximum years
+  minYr <- min(flowDf$Year)
+  maxYr <- max(flowDf$Year)
+  
+  # Figure out the starting year to match the year window
+  startYr <- minYr + yrWindow - 1
+  
+  # Create empty vector
+  dryVector <- numeric(length = nrow(flowDf))
+  #dryVector <- NA
+  
+  # Toggle signifies if we're looking at total/cumulative flow (1) or annual (0)
+  if (totToggle == 0) {
+    # Run through loop of all applicable year where window will work
+    for (i in startYr:maxYr) {
+      # Determine if any of the years meet the described flow conditions
+      if ((any(flowDf$Flow_AF[flowDf$Year<=i & flowDf$Year>(i-yrWindow)] > 
+               dryFlowMin) &
+           any(flowDf$Flow_AF[flowDf$Year<=i & flowDf$Year>(i-yrWindow)] <= 
+               dryFlowMax)) == TRUE) {
+        dryVector[which(flowDf$Year == i)[1]] <- 1
+      } else {dryVector[which(flowDf$Year == i)[1]] <- 0}
+    }
+  } else {
+    for (i in startYr:maxYr) {
+      if (sum(flowDf$Flow_AF[flowDf$Year<=i & flowDf$Year>(i-yrWindow)] > 
+               dryFlowMax)) {
+        dryVector[which(flowDf$Year == i)[1]] <- 1
+      } else {dryVector[which(flowDf$Year == i)[1]] <- 0}
+    }
+  }
+  
+  return(dryVector)
+}
+
 # 1. in df, identify the dry periods defined so that for a window of k = 1, 2, 3, 4 year(s), the minimum of the annual flow in the windows is equal or lower than the minimum of the annual flow during the 2012-2016 drought
+minAflow <- min(df$Flow_AF[df$Year>2011 & df$Year<2017])
+df$Yr1 <- dryPeriods(df, 1, 0, minAflow, 0)
+df$Yr2 <- dryPeriods(df, 2, 0, minAflow, 0)
+df$Yr3 <- dryPeriods(df, 3, 0, minAflow, 0)
+df$Yr4 <- dryPeriods(df, 4, 0, minAflow, 0)
 
 # 2. plot four times series on the same graph for each of the case of 1. i.e. for k = 1, 2, 3, 4
+p1 <- ggplot(df, aes(x = Year, y = Yr1)) +
+  geom_line(color = "red", alpha = 0.2) +
+  geom_line(aes(x = Year, y = Yr2), color = "blue", alpha = 0.2) +
+  geom_line(aes(x = Year, y = Yr3), color = "orange", alpha = 0.2) +
+  geom_line(aes(x = Year, y = Yr3), color = "black", alpha = 0.2) +
+  scale_y_continuous(name = "Dry or Wet Years", breaks = c(0,1),
+                     labels = c("Wet", "Dry"))
+print(p1)
 
 # 3. in df, identify the dry years defined so that the annual flow of a dry year is comprised (inclusively )between the minimum and the maximum of the annual flow of the 2012-2016 drought
+maxAflow <- max(df$Flow_AF[df$Year>2011 & df$Year<2017])
+df$inclusive <- dryPeriods(df, 1, minAflow, maxAflow, 0)
 
 # 4. plot the time series with the dry periods from 3.
+p2 <- ggplot(df, aes(x = Year, y = inclusive)) +
+  geom_line(color = "black", alpha = 0.2) +
+  scale_y_continuous(name = "Dry or Wet Years", breaks = c(0,1),
+                     labels = c("Wet", "Dry"))
+print(p2)
 
-# 5. in df, identify the dry periods so that in a 4-years window the cumulative flow is equal or lower to the cumulative flow during the 2012-2016 drought.
+# 5. in df, identify the dry periods so that in a 5-years window the cumulative flow is equal or lower to the cumulative flow during the 2012-2016 drought.
+totFlow <- sum(df$Flow_AF[df$Year>2011 & df$Year<2017])
+df$cumulative <- dryPeriods(df, 5, 0, totFlow, 1)
 
 # 6. plot the time series with the dry periods from 5.
+p3 <- ggplot(df, aes(x = Year, y = cumulative)) +
+  geom_line(color = "black", alpha = 0.2) +
+  scale_y_continuous(name = "Dry or Wet Years", breaks = c(0,1),
+                     labels = c("Wet", "Dry"))
+print(p3)
